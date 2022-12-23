08a77cb14ac66ca8fb46bbd6c4e07e8f0990f9d3
Nikhil <Sknikhil2000@gmail.com>
2022-12-23 13:18:12 +0530

    tictactoe

---
+++ src/main/java/com/sample/Player.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import javax.management.ConstructorParameters;
+import java.lang.reflect.Constructor;
+
+public class Player {
+    private String character;
+
+    public Player(String character){
+        this.character=character;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
